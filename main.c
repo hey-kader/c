c309bdce6579fca98a66a6707d0e638c3fbd74a3
pablopenrose <local@kader.email>
2021-07-10 23:45:38 -0400

    %c to %d in format string for printf()

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void read_wav (void);
+
 int main (int argc, char * argv[]) {
 
-    FILE * fp = fopen("song.wav", "r");
-    char c;
-    printf("\n");
-    while ((c = fgetc(fp)) != EOF) {
-        printf("%c ", c);
-    }
-    printf("\n");
-    fclose(fp);
-    
+    read_wav();
     return 0;
 
+}
+
+void read_wav () {
+    FILE * fp = fopen("song.wav", "r");
+    int c;
+    long count = 0;
+    printf("\n");
+    while ((c = fgetc(fp)) != EOF) {
+        printf("%c", c);
+        count++;
+    }
+    printf("\ncount: %ld\n", count);
+    fclose(fp);
 }
